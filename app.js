30f02f3082340745f8321a98a2bc8ab027420a41
jbuchanon <jbuch1964@gmail.com>
2021-10-26 12:59:17 -0400

    AtLunch

---
+++ app.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const app = express();
+const port = 3000;
+const movies = require('./mock-data/movies.json');
+
+app.listen(port, () => {
+    console.log(`my server is running at http://localhost:${port}`)
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+/* ---- BELOW ARE APP METHODS ----*/
+
+app.get('/movies', (req, res) => {
+    res.status(200).json(movies);
+});
+
+app.get("/movies/:id", (req, res) => {
+    let movieId = req.params["id"];
+    if (Number.isNaN(parseInt(movieId, 10))) {
+        res.status(400).send("Invalid ID supplied");
+    } else {
+        let movie = movies.filter((elem) => elem.id == movieId);
+
+        if (movie.length) {
+            res.status(200).send([
+                {
+                    title: movie[0].title,
+                    author: movie[0].director,
+                    release_year: movie[0].release_year,
+                },
+            ]);
+        } else {
+            res.status(404).send("Movie ID not found");
+        }
+    }
+});
+
+app.get('/movies/', (req, res) => {
+    //res.status(200).send(req.query.title)
+
+    let titleQ = {};
+    let query = false;
+    console.log("Query value: ", query)
+
+    //ADD a 400 response
+    // 400 text 'Invalid titleQuery supplied'
+
+    req.query.title && (query = true);
+    query && (titleQ = movies.find((movie) => movie.title === req.query.title));
+    console.log(`query: ${Object.entries(req.query)}, titleQ: ${titleQ?.title}, query: ${query}`);
+    (!_.isEqual(titleQ, undefined) && !_.isEqual(titleQ, {})) ? res.send(titleQ) : res.status(404).send('Your query string returned no results.');
+
+});
+
+
+// app.post("/movies", (req, res) => {
+//   movies.push(req.body);
+//   res.status(200).end();
+// });
+
+// app.delete("/movies/:id", (req, res) => {
+//   let movieId = req.params["id"];
+//   if (Number.isNaN(parseInt(movieId, 10))) {
+//     res.status(400).send("Invalid ID supplied");
+//   } else {
+//     let movieFound = -1;
+//     for (let i = 0; i < movies.length; i++) {
+//       if (movies[i].id == movieId) {
+//         movieFound = i;
+//         movies.splice(i, 1);
+//         res.status(200).end();
+//       }
+//     }
+//     if (!movieFound) {
+//       res.status(404).send("Movie ID not found");
+//     }
+//   }
+//   res.end();
+// });
+
